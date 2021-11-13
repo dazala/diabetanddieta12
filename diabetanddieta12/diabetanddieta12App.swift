0b0e6991707a46be7cb453737daf62ae708b8560
dazala <alexeyweb90@gmail.com>
2021-11-13 17:53:11 +0200

    Initial Commit

---
+++ diabetanddieta12/diabetanddieta12App.swift
@@ -0,0 +1,17 @@
+//
+//  diabetanddieta12App.swift
+//  diabetanddieta12
+//
+//  Created by alexey on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct diabetanddieta12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
